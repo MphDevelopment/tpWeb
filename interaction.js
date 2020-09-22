eeb69c8d9604ef9b52f875d0271174766b15ae65
Theo Jammes-Beuve <theo.jammes@gmail.com>
2020-09-22 14:21:00 +0200

    Added interactions

--- interaction.js
+++ interaction.js
@@ -2,11 +2,58 @@
 // La création d'un Dnd requière un canvas et un interacteur.
 // L'interacteur viendra dans un second temps donc ne vous en souciez pas au départ.
 function DnD(canvas, interactor) {
-	// Définir ici les attributs de la 'classe'
+  // Définir ici les attributs de la 'classe'
+
+  this.initialX = 0;
+  this.initialY = 0;
+  
+  this.finalX = 0;
+  this.finalY = 0;
+
+  this.mousePressed = false;
 
 	// Developper les 3 fonctions gérant les événements
+  DnD.prototype.onMouseUp = evt => {
+    if(this.mousePressed){
+      res = getMousePosition(canvas, evt);
+      this.finalX = res.x;
+      this.finalY = res.y;
+      this.mousePressed = false;
+      console.log(this.initialX + " " + this.initialY + " " +  this.finalX + " " + this.finalY);
+      interactor.onInteractionEnd(this);
+    } 
+  }
+  
+  DnD.prototype.onMouseDown = evt => {
+    res = getMousePosition(canvas, evt);
 
-	// Associer les fonctions précédentes aux évènements du canvas.
+    this.initialX = res.x;
+    this.initialY = res.y;
+
+    this.mousePressed = true;
+
+    console.log(this.initialX + " " + this.initialY + " " +  this.finalX + " " + this.finalY)
+
+    interactor.onInteractionStart(this);
+  }
+  
+  DnD.prototype.onMouseMove = evt => {
+    if(this.mousePressed){
+      res = getMousePosition(canvas, evt);
+      this.finalX = res.x;
+      this.finalY = res.y;
+      console.log(this.initialX + " " + this.initialY + " " +  this.finalX + " " + this.finalY)
+
+      interactor.onInteractionUpdate(this);
+    }
+  }
+  
+
+  // Associer les fonctions précédentes aux évènements du canvas.
+  canvas.addEventListener('mousedown', this.onMouseDown, false);
+  canvas.addEventListener('mousemove', this.onMouseMove, false);
+  canvas.addEventListener('mouseup', this.onMouseUp, false);
+
 };
 
 
