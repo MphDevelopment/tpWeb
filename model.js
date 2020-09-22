da9bc4863ac43cb8d04e0d3f10fe0a77a8b57569
Theo Jammes-Beuve <theo.jammes@gmail.com>
2020-09-22 14:21:51 +0200

    Added view and model

--- model.js
+++ model.js
@@ -1,3 +1,41 @@
 
 // Implémenter ici les 4 classes du modèle.
 // N'oubliez pas l'héritage !
+
+function Drawing(){
+    this.shapes = [];
+
+    Drawing.prototype.getShapes = function() {
+        return this.shapes;
+    }
+
+}
+
+function Shape(color, thickness){
+    this.color = color;
+    this.thickness = thickness;
+
+}
+
+function Rectangle(startx, starty, length, width, thickness, color){
+
+    Shape.call(this, color, thickness);
+    this.startx = startx;
+    this.starty = starty;
+    this.length = length;
+    this.width = width;    
+
+}
+Rectangle.prototype = new Shape();
+
+function Line(startx, starty, endx, endy, thickness, color){
+    Shape.call(this, color, thickness);
+    this.startx = startx;
+    this.starty = starty;
+
+    this.endx = endx;
+    this.endy = endy;
+
+}
+Line.prototype = new Shape();
+
--- view.js
+++ view.js
@@ -1,2 +1,35 @@
 
+
+Shape.prototype.paint = function(ctx){
+    ctx.strokeStyle = this.color;
+    ctx.lineWidth = this.thickness;
+    ctx.fillStyle = '#F0F0F0'; // set canvas' background color
+    ctx.fillRect(0, 0, canvas.width, canvas.height);  // now fill the canvas
+}
 // Implémenter ici les fonctions paint à ajouter dans chacune des classes du modèle.
+Rectangle.prototype.paint = function(ctx) {
+    //TODO Manager color
+    Shape.prototype.paint.call(this, ctx)
+    
+    ctx.rect(this.startx, this.starty, this.startx+this.length, this.starty + this.width);
+    ctx.stroke();
+  };
+  
+  Line.prototype.paint = function(ctx) {
+    //TODO Manager color
+    ctx.beginPath();
+    ctx.moveTo(this.startx, this.starty);
+    ctx.lineTo(this.endx, this.endy);
+    ctx.stroke();
+  };
+  
+  Drawing.prototype.paint = function(ctx) {
+    //console.log(this.getForms());
+    ctx.fillStyle = '#F0F0F0'; // set canvas' background color
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    this.getForms().forEach(function (eltDuTableau) {
+      // now fill the canvas
+      eltDuTableau.paint(ctx);
+    });
+  };
+  
